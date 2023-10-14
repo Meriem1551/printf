44a17cda66d25a0326e25f3a0edf67486aa2b3dc
Kawthar Ikram Khezour <kawtharikram2020@gmail.com>
2023-10-14 07:44:14 -0700

    printf

---
+++ test.c/task1.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * convert - Converts an integer to a string in a specified base
+ * @num: The input integer to convert
+ * @base: The base for conversion (e.g., decimal, binary, octal)
+ * @lowercase: A flag indicating whether letters should be lowercase
+ *
+ * Return: The resulting string
+ */
+char *convert(unsigned int num, int base, int lowercase)
+{
+    static char *rep;
+    static char buffer[50];
+    char *ptr;
+
+    // Define the character set for the base (lowercase if specified)
+    rep = (lowercase)
+        ? "0123456789abcdefghijklmnopqrstuvwxyz"
+        : "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    ptr = &buffer[49];
+    *ptr = '\0';
+
+    do {
+        *--ptr = rep[num % base];
+        num /= base;
+    } while (num != 0);
+
+    return ptr;
+}
+
