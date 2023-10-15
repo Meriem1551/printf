5eb4d09e697f7854e42c00e6ea81604a4f5269a9
Khezour Kawthar Ikram <kawtharikram2020@gmail.com>
2023-10-15 14:51:38 +0100

    hello

--- main.h
+++ main.h
@@ -1,43 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef PROTO_DECLARED
+#define PROTO_DECLARED
 #include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * struct format - struct
- * @id: char
- * @f: int
- * Return: nothing
- */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} match;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int _printf(const char *format, ...);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _strlen(const char *str);
-int _strleng(char *str);
-int print_37(void);
-int printf_dec(va_list args);
-int printf_int(va_list args);
-int print_binary(va_list val);
-int print_rot13(va_list val);
-int printf_unsigned_int(va_list args);
-int print_oct(va_list val);
-int print_hextra(unsigned long int num);
-int print_hex(va_list val);
-int print_STRING(va_list val);
-int PRINT_HEX(va_list val);
-int print_HEXtra(unsigned int num);
-int print_pointer(va_list val);
-int print_rev(va_list val);
-int handle_flags(int n, char *flags);
+int resolve_format(char c, va_list printargs);
+int print_string(char *s);
+int print_number(int d, int i);
+int print_unsigned_number(unsigned int d, int i);
 #endif
+
