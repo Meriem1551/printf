87e6e17f759e80f9fe4f1ef66a389a0a25121fa2
Meriem1551 <meriem200409@gmail.com>
2023-10-16 10:47:11 -0700

    Fixing some issues

--- print_rev.c
+++ print_rev.c
@@ -9,15 +9,15 @@
 int printf_srev(va_list args)
 {
 
-	char *s = va_arg(args, char*);
+	char *str = va_arg(args, char*);
 	int i;
 	int j = 0;
 
-	if (s == NULL)
-		s = "(null)";
-	while (s[j] != '\0')
+	if (str == NULL)
+		str = "(null)";
+	while (str[j] != '\0')
 		j++;
 	for (i = j - 1; i >= 0; i--)
-		_putchar(s[i]);
+		_putchar(str[i]);
 	return (j);
 }
