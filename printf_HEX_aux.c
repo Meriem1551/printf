988ae1fe39f107127bea78ae937b8a9016c0ff8b
Meriem1551 <meriem200409@gmail.com>
2023-10-16 11:11:40 -0700

    Create a Func that prints an hexadecimal num

---
+++ printf_HEX_aux.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * printf_HEX_aux - prints an hexgecimal number.
+ * @num: number to print.
+ * Return: counter.
+ */
+int printf_HEX_aux(unsigned int num)
+{
+	int i;
+	int *array;
+	int count = 0;
+	unsigned int tmp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		count++;
+	}
+	count++;
+	array = malloc(count * sizeof(int));
+
+	for (i = 0; i < count; i++)
+	{
+		array[i] = tmp % 16;
+		tmp /= 16;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (count);
+}
