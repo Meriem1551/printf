434a6d2524c7df7377fc61c172cdeb6e39581af6
Khezour Kawthar Ikram <kawtharikram2020@gmail.com>
2023-10-16 11:13:49 -0700

    printf

---
+++ print_octal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * printf_oct - Print octal number.
+ * @val: The argument to be converted and printed.
+ * Return: The number of characters printed.
+ */
+int printf_oct(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
