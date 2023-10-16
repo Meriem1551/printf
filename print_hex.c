2b284e15f1ab1372fe06f93df456fa7f2a042ebd
Meriem1551 <meriem200409@gmail.com>
2023-10-16 11:16:57 -0700

    Creating hex func

---
+++ print_hex.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * printf_hex - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int printf_hex(va_list val)
+{
+	int i;
+	int *array;
+	int count = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tmp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		count++;
+	}
+	counter++;
+	array = malloc(count * sizeof(int));
+
+	for (i = 0; i < count; i++)
+	{
+		array[i] = tmp % 16;
+		temp /= 16;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (count);
+}
