8d015e9c1681accb0c54fecd7c4181cb41471dc9
Khezour Kawthar Ikram <kawtharikram2020@gmail.com>
2023-10-16 10:28:32 -0700

    printf

--- print_string.c
+++ print_string.c
@@ -11,23 +11,22 @@
  */
 int printf_string(va_list val)
 {
-        char *s;
-        int i, len;
-
-        s = va_arg(val, char *);
-        if (s == NULL)
-        {
-                s = "(null)";
-                len = _strlen(s);
-                for (i = 0; i < len; i++)
-                        _putchar(s[i]);
-                return (len);
-        }
-        else
-        {
-                len = _strlen(s);
-                for (i = 0; i < len; i++)
-                        _putchar(s[i]);
-                return (len);
-        }
+char *s;
+int i, len;
+s = va_arg(val, char *);
+if (s == NULL)
+{
+s = "(null)";
+len = _strlen(s);
+for (i = 0; i < len; i++)
+_putchar(s[i]);
+return (len);
+}
+else
+{
+len = _strlen(s);
+for (i = 0; i < len; i++)
+_putchar(s[i]);
+return (len);
+}
 }
