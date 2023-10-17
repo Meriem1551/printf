5a54b37d5bf5ea88d675ad801860de3b1bd86e85
Meriem1551 <meriem200409@gmail.com>
2023-10-16 21:48:42 -0700

    Declaring missing func

--- main.h
+++ main.h
@@ -44,4 +44,19 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 char *convert(unsigned long int num, int base, int lowercase);
 int _puts(char *str);
+int print_address(va_list l, flags_t *f);
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
 #endif /* MAIN_H */
